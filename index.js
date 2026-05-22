232e179ade64d1970a9ee9a5513be9aece20f1e1
AmerAhmed777 <amer@example.com>
2026-05-22 22:37:31 +0300

    تغيير معرف الجلسة لكسر تضارب التشفير

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function clearAllQueues() {
 
 async function startBot() {
     await redis.flushall();
-    const { state, saveCreds } = await useRedisAuthState(redis, 'wa_session_5');
+    const { state, saveCreds } = await useRedisAuthState(redis, 'wa_session_6');
     
     const sock = makeWASocket({ 
         auth: state, 
