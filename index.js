29a9559e5286f9f74ddc2b20d3bb5584eb2484b6
AmerAhmed777 <amer@example.com>
2026-05-24 22:43:06 +0300

    fix model name

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function askGemini(jid, prompt) {
         let fullPrompt = historyArray.join('\n') + '\nالمستخدم: ' + prompt;
 
         const response = await axios.post(
-            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${aiKey}`,
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${aiKey}`,
             { contents: [{ parts: [{ text: fullPrompt }] }] },
             { headers: { 'Content-Type': 'application/json' }, timeout: 15000 }
         );
